5d8e20df7b11c0a45d787a7c27e0f01a79be5efb
Haitham Gado <taegado@gmail.com>
2025-01-30 08:12:19 +0200

    GetCountriesUseCaseProtocol

---
+++ WorldFinder/WorldFinder/Domain/Models/ErrorMessage.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorMessage.swift
+//  WorldFinder
+//
+//  Created by Haitham Gado on 25/01/2025.
+//
+
+import Foundation
+
+struct ErrorMessage: Identifiable {
+    let id = UUID()
+    let message: String  
+}
